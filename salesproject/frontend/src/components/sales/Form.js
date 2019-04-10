ad3f65c93fc3d4a2ed1d7dfb445804b16f47a3da
courtneywilliams561@gmail.com <36859589+kwame561@users.noreply.github.com>
2019-04-09 21:38:11 -0400

    Updated the layout of the form

--- salesproject/frontend/src/components/sales/Form.js
+++ salesproject/frontend/src/components/sales/Form.js
@@ -47,6 +47,17 @@ export class Form extends Component {
       tools_used
     };
     this.props.addSale(sale);
+    this.setState({
+      title = "",
+      name_of_client = "",
+      email = "",
+      category = "",
+      sub_category = "",
+      description = "",
+      total_duration = "",
+      total_price = "",
+      tools_used = ""
+    })
   };
 
   render() {
@@ -66,55 +77,62 @@ export class Form extends Component {
       <div className="card card-body mt-4 mb-4">
         <h2>Add Lead</h2>
         <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Title</label>
-            <input
-              className="form-control"
-              type="text"
-              name="title"
-              onChange={this.onChange}
-              value={title}
-            />
+          <div className="form-row">
+            <div className="form-group col">
+              <label>Title</label>
+              <input
+                className="form-control"
+                type="text"
+                name="title"
+                placeholder="Title"
+                onChange={this.onChange}
+                value={title}
+              />
+            </div>
+            <div className="form-group col">
+              <label>Name of Client</label>
+              <input
+                className="form-control"
+                type="text"
+                name="name_of_client"
+                placeholder="Name of Client"
+                onChange={this.onChange}
+                value={name_of_client}
+              />
+            </div>
           </div>
-          <div className="form-group">
-            <label>Name of Client</label>
-            <input
-              className="form-control"
-              type="text"
-              name="name_of_client"
-              onChange={this.onChange}
-              value={name_of_client}
-            />
-          </div>
-          <div className="form-group">
-            <label>Email</label>
-            <input
-              className="form-control"
-              type="email"
-              name="email"
-              onChange={this.onChange}
-              value={email}
-            />
-          </div>
-          <div className="form-group">
-            <label>Category</label>
-            <input
-              className="form-control"
-              type="text"
-              name="category"
-              onChange={this.onChange}
-              value={category}
-            />
-          </div>
-          <div className="form-group">
-            <label>Sub-Category</label>
-            <input
-              className="form-control"
-              type="text"
-              name="sub_category"
-              onChange={this.onChange}
-              value={sub_category}
-            />
+          <div className="form-row">
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                className="form-control col"
+                type="email"
+                name="email"
+                placeholder="johnsmith@domain.com"
+                onChange={this.onChange}
+                value={email}
+              />
+            </div>
+            <div className="form-group col">
+              <label>Category</label>
+              <input
+                className="form-control"
+                type="text"
+                name="category"
+                onChange={this.onChange}
+                value={category}
+              />
+            </div>
+            <div className="form-group col">
+              <label>Sub-Category</label>
+              <input
+                className="form-control"
+                type="text"
+                name="sub_category"
+                onChange={this.onChange}
+                value={sub_category}
+              />
+            </div>
           </div>
           <div className="form-group">
             <label>Description</label>
@@ -122,41 +140,45 @@ export class Form extends Component {
               className="form-control"
               type="text"
               name="description"
+              placeholder="Short description of the project..."
               onChange={this.onChange}
               value={description}
             />
           </div>
-          <div className="form-group">
-            <label>Total Duration</label>
-            <textarea
-              className="form-control"
-              type="number"
-              name="total_duration"
-              step=".01"
-              onChange={this.onChange}
-              value={total_duration}
-            />
-          </div>
-          <div className="form-group">
-            <label>Total Price</label>
-            <textarea
-              className="form-control"
-              type="number"
-              name="total_price"
-              step=".01"
-              onChange={this.onChange}
-              value={total_price}
-            />
-          </div>
-          <div className="form-group">
-            <label>Tools Used</label>
-            <textarea
-              className="form-control"
-              type="text"
-              name="tools_used"
-              onChange={this.onChange}
-              value={tools_used}
-            />
+          <div className="form-row">
+            <div className="form-group col">
+              <label>Total Duration</label>
+              <textarea
+                className="form-control"
+                type="number"
+                name="total_duration"
+                step=".01"
+                onChange={this.onChange}
+                value={total_duration}
+              />
+            </div>
+            <div className="form-group col">
+              <label>Total Price</label>
+              <textarea
+                className="form-control"
+                type="number"
+                name="total_price"
+                step=".01"
+                onChange={this.onChange}
+                value={total_price}
+              />
+            </div>
+            <div className="form-group col">
+              <label>Technology Used</label>
+              <textarea
+                className="form-control"
+                type="text"
+                name="tools_used"
+                placeholder="e.g. Python, R, etc."
+                onChange={this.onChange}
+                value={tools_used}
+              />
+            </div>
           </div>
           {/* Submit button */}
           <div className="form-group">
